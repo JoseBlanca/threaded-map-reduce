903b401fdfee2409763f1b6764b7ddb2f8985dcb
Jose Blanca <jblanca@upv.es>
2025-07-20 15:25:04 +0200

    working version with computing threads

--- src/threaded_map_reduce/threaded_map_reduce.py
+++ src/threaded_map_reduce/threaded_map_reduce.py
@@ -1,25 +1,10 @@
 from typing import Iterable
 from concurrent.futures import ThreadPoolExecutor, Future
 from itertools import chain
+import itertools
 import threading
 import queue
-
-
-def _pairwise(iterable):
-    it = iter(iterable)
-    while True:
-        try:
-            a = next(it)
-            print("a", a)
-        except StopIteration:
-            break
-        try:
-            b = next(it)
-            print("b", b)
-        except StopIteration:
-            yield (a,)
-            continue
-        yield a, b
+import functools
 
 
 def _create_future_from_value(value):
@@ -137,7 +122,7 @@ class _MapReducer:
         return futures_created
 
 
-def map_reduce(map_fn, reduce_fn, iterable: Iterable, max_workers: int):
+def map_reduce2(map_fn, reduce_fn, iterable: Iterable, max_workers: int):
     if True:
         map_reducer = _MapReducer(map_fn, reduce_fn, iterable, max_workers)
         return map_reducer.map_sum
@@ -153,3 +138,215 @@ def map_reduce(map_fn, reduce_fn, iterable: Iterable, max_workers: int):
             future.add_done_callback(_ended)
             print("waiting")
             future.result()
+
+
+def _map_reduce_chunk(map_fn, reduce_fn, items):
+    mapped_items = map(map_fn, items)
+    return functools.reduce(reduce_fn, mapped_items)
+
+
+def _get_n_items(items, num_items):
+    return itertools.islice(items, num_items)
+
+
+def _materialize_futures(futures_queue):
+    while True:
+        try:
+            future = futures_queue.get()
+        except queue.ShutDown:
+            break
+        yield future.result()
+
+
+def _put_tasks_in_queue(tasks, executor, futures_queue):
+    for task in tasks:
+        future = executor.submit(task["function"], *task["args"])
+        futures_queue.put(future)
+
+
+def _shutdown_queue(queue_):
+    queue_.shutdown()
+    print("closing")
+
+
+def _map_reduce_chunks(map_fn, reduce_fn, items, num_items_per_chunk, max_workers):
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures_queue = queue.Queue(maxsize=max_workers)
+
+        chunks = _create_chunks(items, num_items_per_chunk)
+        tasks = (
+            {
+                "function": _map_reduce_chunk,
+                "args": (
+                    map_fn,
+                    reduce_fn,
+                    chunk,
+                ),
+            }
+            for chunk in chunks
+        )
+        task_submitting_thread1 = threading.Thread(
+            target=_put_tasks_in_queue,
+            args=(tasks, executor, futures_queue),
+        )
+        task_submitting_thread1.start()
+
+        intermediate_results = list(_materialize_futures(futures_queue))
+        chunks = _create_chunks(intermediate_results, num_items_per_chunk)
+        reduce_tasks = [
+            {
+                "function": functools.reduce,
+                "args": (
+                    reduce_fn,
+                    chunk,
+                ),
+            }
+            for chunk in chunks
+        ]
+        closing_task = {"function": _shutdown_queue, "args": (futures_queue,)}
+        reduce_tasks.append(closing_task)
+        task_submitting_thread2 = threading.Thread(
+            target=_put_tasks_in_queue,
+            args=(reduce_tasks, executor, futures_queue),
+        )
+        task_submitting_thread2.start()
+        while True:
+            try:
+                future = futures_queue.get()
+                print(future.result())
+            except queue.ShutDown:
+                print("closing")
+                break
+
+
+def map_reduce3(map_fn, reduce_fn, iterable: Iterable, max_workers: int):
+    items = iter(iterable)
+    materialized_chunk_len = 10000
+    _map_reduce_chunks(map_fn, reduce_fn, items, materialized_chunk_len, max_workers)
+
+
+def _pairwise(iterable):
+    it = iter(iterable)
+    while True:
+        try:
+            a = next(it)
+            print("a", a)
+        except StopIteration:
+            break
+        try:
+            b = next(it)
+            print("b", b)
+        except StopIteration:
+            yield (a,)
+            continue
+        yield a, b
+
+
+def _create_chunks(
+    items,
+    num_items_per_chunk,
+):
+    while True:
+        chunk = list(_get_n_items(items, num_items_per_chunk))
+        if not chunk:
+            break
+        yield chunk
+
+
+def _map_reduce_items(items, map_fn, reduce_fn):
+    mapped_items = map(map_fn, items)
+    return functools.reduce(reduce_fn, mapped_items)
+
+
+def _send_map_reduce_tasks(map_reduce_fn, items, executor):
+    for item in items:
+        future = executor.submit(map_reduce_fn, item)
+        yield future
+
+
+def simple_map_reduce(
+    map_fn, reduce_fn, iterable: Iterable, max_workers: int, num_items_per_chunk=1000
+):
+    items = iter(iterable)
+
+    executor = ThreadPoolExecutor(max_workers=max_workers)
+
+    items_chunks = _create_chunks(
+        items,
+        num_items_per_chunk,
+    )
+    map_reduce_items = functools.partial(
+        _map_reduce_items, map_fn=map_fn, reduce_fn=reduce_fn
+    )
+
+    first_round_map_reduce_futures = _send_map_reduce_tasks(
+        map_reduce_items, items_chunks, executor
+    )
+    first_round_map_reduce_results = (
+        future.result() for future in first_round_map_reduce_futures
+    )
+
+    result = functools.reduce(reduce_fn, first_round_map_reduce_results)
+    print(result)
+    return result
+
+
+def _get_items_from_queue(items_queue, results_queue):
+    while True:
+        try:
+            yield items_queue.get()
+        except queue.ShutDown:
+            break
+
+
+def _map_reduce_items_from_queue(items_queue, results_queue, map_fn, reduce_fn):
+    result = functools.reduce(
+        reduce_fn, map(map_fn, _get_items_from_queue(items_queue, results_queue))
+    )
+    results_queue.put(result)
+
+
+def _feed_items(items, items_queue):
+    for item in items:
+        items_queue.put(item)
+    items_queue.shutdown()
+
+
+def map_reduce_with_thread_pool(
+    map_fn, reduce_fn, iterable: Iterable, max_workers: int
+):
+    num_threads = max_workers
+    items = iter(iterable)
+    items_queue = queue.Queue()
+    results_queue = queue.Queue()
+    map_reduce_items = functools.partial(
+        _map_reduce_items_from_queue,
+        map_fn=map_fn,
+        reduce_fn=reduce_fn,
+        items_queue=items_queue,
+        results_queue=results_queue,
+    )
+
+    computing_threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=map_reduce_items)
+        thread.start()
+        computing_threads.append(thread)
+
+    item_feeder_thread = threading.Thread(target=_feed_items, args=(items, items_queue))
+    item_feeder_thread.start()
+
+    results = []
+    while True:
+        result = results_queue.get()
+        results.append(result)
+        if len(results) == len(computing_threads):
+            results_queue.shutdown()
+            break
+
+    result = functools.reduce(reduce_fn, results)
+
+    return result
+
+
+map_reduce = map_reduce_with_thread_pool
