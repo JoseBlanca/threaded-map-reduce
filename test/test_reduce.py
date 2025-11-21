68512f8836330173ad2e75c132fcc1c741a39bce
Jose Blanca <jblanca@upv.es>
2025-11-21 18:48:47 +0100

    fix tests

--- test/test_reduce.py
+++ test/test_reduce.py
@@ -14,7 +14,7 @@ def test_map_reduce():
     squares = map(square, nums)
     result1 = funct_reduce(add, squares)
     nums = range(num_items)
-    result2 = map_reduce(square, add, nums, num_computing_threads=4, buffer_size=10)
+    result2 = map_reduce(square, add, nums, num_computing_threads=4, chunk_size=10)
     assert result1 == result2
 
 
@@ -22,5 +22,5 @@ def test_map():
     num_items = 101
     nums = range(num_items)
     squares = map(square, nums)
-    squares2 = threaded_map(square, nums, max_workers=2)
+    squares2 = threaded_map(square, nums, num_computing_threads=2)
     assert list(squares) == list(squares2)
