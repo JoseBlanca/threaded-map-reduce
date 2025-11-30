53ccf4e65185039880b90ba95eb90d7771b11e37
Jose Blanca <jblanca@upv.es>
2025-11-30 08:54:14 +0100

    test is_prime function

--- performance/performance_utils.py
+++ performance/performance_utils.py
@@ -42,6 +42,56 @@ def is_prime(n):
 
 
 def test_is_prime():
-    primes = [n for n in range(1, 21) if is_prime(n)]
-    assert primes == [2, 3, 5, 7, 11, 13, 17, 19]
-    print(primes)
+    primes = [n for n in range(1, 200) if is_prime(n)]
+    assert primes == [
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        41,
+        43,
+        47,
+        53,
+        59,
+        61,
+        67,
+        71,
+        73,
+        79,
+        83,
+        89,
+        97,
+        101,
+        103,
+        107,
+        109,
+        113,
+        127,
+        131,
+        137,
+        139,
+        149,
+        151,
+        157,
+        163,
+        167,
+        173,
+        179,
+        181,
+        191,
+        193,
+        197,
+        199,
+    ]
+
+
+if __name__ == "__main__":
+    test_is_prime()
